cb4f764ac245825a6d09e049d7a949bb642ef45c
uvsq21601041 <enze.wu@ens.uvsq.fr>
2020-04-20 20:48:32 +0200

    init

---
+++ src/main/java/pglp5_2/pglp5_2/App.java
@@ -0,0 +1,13 @@
+package pglp5_2.pglp5_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
